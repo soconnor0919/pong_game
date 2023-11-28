bc64dc9e7a4a09707479e5247e181bd2946c7aff
Sean O'Connor <sean@soconnor.dev>
2023-11-28 10:58:44 -0500

    Update README.md and comments

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # Pong Game
 
-## By Sean O'Connor, Fall 2023 (CSCI 201)
+### By Sean O'Connor, Fall 2023 (CSCI 201)
 
 ### Project Summary:
 This is a simple implementation of a pong game written using the [p5.js](https://p5js.org/) library. The game is single player, where the player controls a paddle and tries to hit the ball past the computer's paddle. The game ends when one player scores 10 points.
+
+It can be played [here](https://soconnor0919.github.io/pong-game/).
+
+### Challenges Faced:
+While creating this program, I had some to learn how to use the p5.js framework, as well as figure out how to implement the game logic. I also had to become comfortable with the p5.js library to draw the game objects to the screen, which took some time. Other than the initial learning curve, the framework was easy to pick up, as I had prior experience using JavaScript and working with web-based applications in general.
 
 ### Resources Used:
 - [p5.js](https://p5js.org/)
--- sketch.js
+++ sketch.js
@@ -1,27 +1,32 @@
+// Variables to store game elements and scores
 let ball;
 let leftPaddle;
 let rightPaddle;
 let leftScore = 0;
 let rightScore = 0;
-let gameSpeed = 1.5;
+let gameSpeed = 1.5; // Initial game speed
 
-let isGameOver = false;
+let isGameOver = false; // Flag to indicate if the game is over
 
+// Set up the canvas and initialize game objects
 function setup() {
-  createCanvas(800, 600);
-  textSize(24);
-  ball = new Ball();
-  leftPaddle = new Paddle(true);
-  rightPaddle = new Paddle(false);
+  createCanvas(800, 600); // Create a canvas with dimensions 800x600 pixels
+  textSize(24); // Set the default text size
+  ball = new Ball(); // Create a new ball object
+  leftPaddle = new Paddle(true); // Create a left paddle object
+  rightPaddle = new Paddle(false); // Create a right paddle object
 }
 
+// Main game loop
 function draw() {
-  background(220);
+  background(220); // Set the background color
 
+  // Check if either player has reached the winning score
   if (leftScore >= 10 || rightScore >= 10) {
     isGameOver = true;
   }
 
+  // Display game over screen if the game is over
   if (isGameOver) {
     displayGameOver();
   } else {
@@ -39,13 +44,13 @@ function draw() {
     ball.checkPaddleCollision(leftPaddle);
     ball.checkPaddleCollision(rightPaddle);
 
-    // Update scores
+    // Update scores on the screen
     fill(0);
     textSize(32);
     text(leftScore, width / 4, 40);
     text(rightScore, 3 * width / 4, 40);
 
-    // Check for scoring
+    // Check for scoring and update game speed
     if (ball.offScreen()) {
       if (ball.x < 0) {
         rightScore++;
@@ -53,19 +58,21 @@ function draw() {
         leftScore++;
       }
       gameSpeed += 0.2; // Increment game speed after a point is scored
-      resetGame();
+      resetGame(); // Reset game state for the next round
     }
   }
 }
 
+// Function to reset the game state
 function resetGame() {
   ball.reset();
   leftPaddle.reset();
   rightPaddle.reset();
 }
 
+// Function to display the game over screen
 function displayGameOver() {
-  fill(255, 0, 0);
+  fill(255, 0, 0); // Set text color to red
   textSize(48);
   textAlign(CENTER, CENTER);
   text('Game Over!', width / 2, height / 2 - 40);
@@ -77,8 +84,10 @@ function displayGameOver() {
   text('Click to play again', width / 2, height / 2 + 100);
 }
 
+// Function to handle mouse click event
 function mousePressed() {
   if (isGameOver) {
+    // Reset game state if the game is over
     isGameOver = false;
     leftScore = 0;
     rightScore = 0;
@@ -87,26 +96,29 @@ function mousePressed() {
   }
 }
 
+// Paddle class representing the left and right paddles
 class Paddle {
   constructor(isLeft) {
-    this.w = 10;
-    this.h = 80;
-    this.y = height / 2 - this.h / 2;
-    this.isLeft = isLeft;
-    this.speed = 6;
+    this.w = 10; // Paddle width
+    this.h = 80; // Paddle height
+    this.y = height / 2 - this.h / 2; // Initial y-coordinate
+    this.isLeft = isLeft; // Flag to determine if it's the left paddle
+    this.speed = 6; // Paddle movement speed
   }
 
+  // Display the paddle on the canvas
   show() {
-    fill(0);
+    fill(0); // Set fill color to black
     noStroke();
     rect(this.isLeft ? 0 : width - this.w, this.y, this.w, this.h);
   }
 
+  // Move the paddle based on user input (for the left paddle) or ball position (for the right paddle)
   move() {
     if (this.isLeft) {
-      this.y = mouseY - this.h / 2;
+      this.y = mouseY - this.h / 2; // Follow the mouse for the left paddle
     } else {
-      // Simple logic for the right paddle
+      // Simple logic for the right paddle to track the ball
       let target = ball.y - this.h / 2;
       this.y += (target - this.y) * 0.14;
     }
@@ -115,44 +127,50 @@ class Paddle {
     this.y = constrain(this.y, 0, height - this.h);
   }
 
+  // Reset the paddle position to the center
   reset() {
     this.y = height / 2 - this.h / 2;
   }
 }
 
+// Ball class representing the game ball
 class Ball {
   constructor() {
-    this.reset();
+    this.reset(); // Initialize ball properties
   }
 
+  // Display the ball on the canvas
   show() {
-    fill(0);
+    fill(0); // Set fill color to black
     noStroke();
     ellipse(this.x, this.y, this.diameter, this.diameter);
   }
 
+  // Move the ball based on its speed
   move() {
     this.x += this.xSpeed * gameSpeed;
     this.y += this.ySpeed * gameSpeed;
 
     // Bounce off the top and bottom edges
     if (this.y < 0 || this.y > height) {
-      this.ySpeed *= -1;
+      this.ySpeed *= -1; // Reverse the y-speed to bounce the ball
     }
   }
 
+  // Reset the ball position and speed
   reset() {
     this.x = width / 2;
     this.y = height / 2;
-    this.diameter = 20;
-    this.xSpeed = random(3, 5) * (random() > 0.5 ? 1 : -1);
-    this.ySpeed = random(2, 4) * (random() > 0.5 ? 1 : -1);
+    this.diameter = 20; // Ball diameter
+    this.xSpeed = random(3, 5) * (random() > 0.5 ? 1 : -1); // Random initial x-speed
+    this.ySpeed = random(2, 4) * (random() > 0.5 ? 1 : -1); // Random initial y-speed
   }
 
   offScreen() {
     return this.x < 0 || this.x > width;
   }
 
+  // Check for collisions with paddles and change direction accordingly
   checkPaddleCollision(paddle) {
     let halfPaddleHeight = paddle.h / 2;
     let halfPaddleWidth = paddle.w / 2;
@@ -161,11 +179,11 @@ class Ball {
     if (this.y + this.diameter / 2 > paddle.y && this.y - this.diameter / 2 < paddle.y + paddle.h) {
       // Check if the ball is within the horizontal bounds of the left paddle
       if (paddle.isLeft && this.x - this.diameter / 2 < paddle.w) {
-        this.xSpeed *= -1;
+        this.xSpeed *= -1; // Reverse the x-speed to bounce the ball
       }
       // Check if the ball is within the horizontal bounds of the right paddle
       else if (!paddle.isLeft && this.x + this.diameter / 2 > width - paddle.w) {
-        this.xSpeed *= -1;
+        this.xSpeed *= -1; // Reverse the x-speed to bounce the ball
       }
     }
   }
